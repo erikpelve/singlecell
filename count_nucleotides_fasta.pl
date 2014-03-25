7c1dadc044f7ea220e3c1de7244ae30e6572670e
erikpelve <erik.pelve@gmail.com>
2014-03-24 23:17:13 -0400

    Update count_nucleotides_fasta.pl
    Debug

--- count_nucleotides_fasta.pl
+++ count_nucleotides_fasta.pl
@@ -10,20 +10,37 @@ my $infile1 = $ARGV[0];
 open my $inhandle1, '<', $infile1 or die ("couldn't open the file $infile1");
 
 
-
-
-
-
+my $s = 1; 
+my $n = 0;
+my %characters;
+my $name;
 while (my $line = <$inhandle1>){ #Loop through fasta file
 	$line =~ s/\r|\n//g;
 	chomp $line;
+
 	if (substr($line, 0,1) eq '>' ){
-		print $line, "\t"
-		
-
-
+		if ($s==0){
+			print $n, "\t";
+			foreach (sort keys %($characters{$name})){
+				print $_, ": $characters{$name}{$_}\t"
 				}
-
-
-
+			$n =0;
+			print "\n";
+			
+		}
+		$name = $line;
+		print $line, "\t";
+		$s =0;
+	}else{
+		my @count = split("", $line);
+		foreach(@count){
+			$n++;
+			if (exists $characters{$name}{$_}){
+				$characters{$name}{$_} = $characters{$name}{$_} +1;
+				}else{
+					$characters{$name}{$_}= 0;}
+				
+			}
+	
+			$n++;}
 }
