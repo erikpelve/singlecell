d3c5fac9854191a4a7111199a81a5c9f951459fa
erikpelve <erik.pelve@gmail.com>
2014-04-07 11:59:21 -0400

    Update treename_search_and_replace_v2.pl
    Added formating of NCBI genebank output. Ready for debug

--- treename_search_and_replace_v2.pl
+++ treename_search_and_replace_v2.pl
@@ -27,14 +27,20 @@ while (my $line = <$inhandle_key>){
 	my @list = split(/\t/, $line);
 	die ("Problem with the key file. Did you add the files in the right order? First key, then tree.") if ($list[1] eq "");
 	my $newname;
-	my @id = split(/>|\.|;/, $list[1]);	
+	my @id = split(/>|\.|;/, $list[1]);
 	my @name_list = split(/;/, $list[1]);
-		if (exists $name_list[1]){
+	my @name_list2 = split(/|/, $list[1];
+		if (exists $name_list[1]){ #The input from sequences retreived from SILVA
 			my @id_cell3 = split(/ /, $id[3]);
 			$newname = join("_", $id[$level], $id[1], $id[2], $id_cell3[0]);
 			$newname =~s/ /_/g;
 			}
-		else{
+		elsif(exists $name_list2[1]);{ #Input from genebank
+			my @id_cell3 = split(/ /, $id[-1]);
+			$newname = join("_", $id[1], $id_cell3[0], $id_cell3[1],$id_cell3[2],$id_cell3[3],$id_cell3[4]);
+			$newname =~s/ /_/g;
+		
+			}else{ #The input from my own sequencing
 		$newname = $id[1];
 			}
 	my @key_id = split(/>/, $list[0]);	
