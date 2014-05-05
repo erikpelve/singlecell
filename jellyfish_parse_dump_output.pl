689acde4a261b3dacc14f9b6810c88ed0a000c38
erikpelve <erik.pelve@gmail.com>
2014-05-05 12:03:02 -0400

    Update jellyfish_parse_dump_output.pl
    Done

--- jellyfish_parse_dump_output.pl
+++ jellyfish_parse_dump_output.pl
@@ -4,41 +4,44 @@ use warnings;
 
 #jellyfish_parse_dump_output.pl
 #Fasta files from the jellyfish dump command ARGV[0] - ...
+#Erik Pelve May 2014
 
-my $outfile = "otuput.jellyfish.dump.table";
-open my $outhandle, ">", $outifle or die "Couldn't write to the file $outfile\n";
-print outhandle "\t";
+
+my $outfile = "output.jellyfish.dump.table";
+open my $outhandle, ">", $outfile or die "Couldn't write to the file $outfile\n";
+#print $outhandle "\t";
 
 my %contigs;
 my %kmers;
 
+
+#Cycle each input fasta file
 foreach(@ARGV){
   open my $inhandle, '<', $_ or die "Couldn't open the file $_\n";
  # print $outhandle $_, "\n";
-  
+        my $freq;
+      my $kmer;
   
   
   #Store data in hash
-      my $freq;
-      my $kmer;
+
       while (my $line = <$inhandle>){
       
        chomp $line;
        if (substr($line, 0,1) eq '>' ){  #header
-        my @list = split(/">"/, $line);
+        my @list = split(/>/, $line);
          $freq = $list[1];
-        }else{
+      	        }else{
         $kmer = $line;  
-        
+        $contigs{$_}{$kmer} = $freq;
+
         #Hash of all kmers
         if (exists $kmers{$line}){
           }else{
             $kmers{$line} = 1;
-            }
-        
-        $contigs{$infile}{kmer} = $line;
+            }       
        }
-        
+ 
     }
 
 
@@ -57,14 +60,16 @@ foreach my $contig (sort keys %contigs){
 #print kmer frequency
 foreach my $kmer (sort keys %kmers){
   print $outhandle $kmer, "\t";
-  foreach my $contig (sort keys %contigs){
-      if (exists $contigs{$contig}{$kmer}){
-        print $contigs{$contig}{$kmer}, "\t";
+ foreach my $contig (sort keys %contigs){
+     if (exists $contigs{$contig}{$kmer}){
+ # 		print $outhandle "XX", "\t";
+        print $outhandle $contigs{$contig}{$kmer}, "\t";
         }else{
-          print $outhandle 0, "\t";
+          print $outhandle "0", "\t";
           }
-      print $outhandle "\n" 
+
   }
+     print $outhandle "\n" 
 }
 
 
