c7a5960bbccbab78512f1fbb840bc9e9e1a4614c
erikpelve <erik.pelve@gmail.com>
2014-05-05 11:24:06 -0400

    Update jellyfish_parse_dump_output.pl
    Ready for debug

--- jellyfish_parse_dump_output.pl
+++ jellyfish_parse_dump_output.pl
@@ -5,8 +5,70 @@ use warnings;
 #jellyfish_parse_dump_output.pl
 #Fasta files from the jellyfish dump command ARGV[0] - ...
 
+my $outfile = "otuput.jellyfish.dump.table";
+open my $outhandle, ">", $outifle or die "Couldn't write to the file $outfile\n";
+print outhandle "\t";
+
+my %contigs;
+my %kmers;
+
 foreach(@ARGV){
+  open my $inhandle, '<', $_ or die "Couldn't open the file $_\n";
+ # print $outhandle $_, "\n";
+  
+  
+  
+  #Store data in hash
+      my $freq;
+      my $kmer;
+      while (my $line = <$inhandle>){
+      
+       chomp $line;
+       if (substr($line, 0,1) eq '>' ){  #header
+        my @list = split(/">"/, $line);
+         $freq = $list[1];
+        }else{
+        $kmer = $line;  
+        
+        #Hash of all kmers
+        if (exists $kmers{$line}){
+          }else{
+            $kmers{$line} = 1;
+            }
+        
+        $contigs{$infile}{kmer} = $line;
+       }
+        
+    }
+
+
   }
-my $infile = $ARGV[0];
-my @inname = split(/\//, $infile1);
-open my $inhandle1, '<', $infile1 or die "Couldn't open the file $infile1\n";
+
+
+
+#print headline
+print $outhandle "\t"; 
+foreach my $contig (sort keys %contigs){
+   print $outhandle $contig, "\t";
+}
+  print $outhandle "\n"; 
+
+
+#print kmer frequency
+foreach my $kmer (sort keys %kmers){
+  print $outhandle $kmer, "\t";
+  foreach my $contig (sort keys %contigs){
+      if (exists $contigs{$contig}{$kmer}){
+        print $contigs{$contig}{$kmer}, "\t";
+        }else{
+          print $outhandle 0, "\t";
+          }
+      print $outhandle "\n" 
+  }
+}
+
+
+
+print "Done! Output in the file $outfile\n\n";
+
+
