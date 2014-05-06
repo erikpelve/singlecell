74def6099e791b930285b5b792bce5e7225ccaaf
erikpelve <erik.pelve@gmail.com>
2014-05-06 10:54:35 -0400

    Create split_multi_fasta_and_split_contigs_in_pieces.pl
    Framework

---
+++ split_multi_fasta_and_split_contigs_in_pieces.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+#split_multi_fasta_and_split_contigs_in_pieces.pl
+#Multi fasta file ARGV[0], bp size of pieces (default = 1000 bp) ARGV[1]. Contigs smaller than SIZE bp are left as they are.
+
+
+my $infile1 = $ARGV[0];
+my @inname = split(/\//, $infile1);
+open my $inhandle1, '<', $infile1 or die "Couldn't open the file $infile1\n";
+
+
+my %fasta_id;
+
+my $outhandle;
+while (my $line = <$inhandle1>){
+	$line =~ s/\r|\n//g; #Remove newlines, regardless of format
+	chomp $line;
+	if (substr($line, 0,1) eq '>' ){  #header
+	  my @list = split(/\>|" "/, $line);
+	  my $filename = $list[1]."_".$inname[-1];
+	  open $outhandle, '>', $filename or die ("Couldn't write to the file $outhandle");
+	  print $outhandle $line, "\n";
+
+	}else{ #body
+		print $outhandle $line, "\n";
+
+	}
+}
