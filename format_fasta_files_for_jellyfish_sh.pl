4421f7ad480ced9bf3ccf4a712c6b2e486c55699
erikpelve <erik.pelve@gmail.com>
2014-05-06 13:23:29 -0400

    Create format_fasta_files_for_jellyfish_sh.pl
    framework

---
+++ format_fasta_files_for_jellyfish_sh.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+#format_fasta_files_for_jellyfish_sh.pl
+#ARGV[0] - list of fasta files without search path
+#ARGV[1] - list of fasta files with search path
