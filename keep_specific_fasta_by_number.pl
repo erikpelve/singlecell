619cd67dfe27750c81ba59f7259b1ff4a921e467
erikpelve <erik.pelve@gmail.com>
2014-05-07 15:18:37 -0400

    Create keep_specific_fasta_by_number.pl

---
+++ keep_specific_fasta_by_number.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+#keep_fasta.pl
+# ARGV [0] - number of fasta sequence in file to keep. 
+#Fasta files ARGV[1] ...
+
+#ARGV[2] = new file name (default "gene.N.fasta")
+
+
+foreach(ARGV)
+
+my $infile1 = $ARGV[0];
+my $infile2 = $ARGV[1];
+my @inname = split(/\//, $infile1);
+my $outfile1 = "subset.".$inname[-1];
+$outfile1 = $ARGV[2] if (exists $ARGV[2]);
+
+open my $inhandle1, '<', $infile1 or die "Couldn't open the file $infile1\n";
+open my $inhandle2, '<', $infile2 or die "Couldn't open the file $infile2\n";
+open my $outhandle, '>', $outfile1 or die "Couldn't write to the file $outfile1\n";
+
+my %fasta_id;
+
+#Read file of fasta names to include
+while (my $line = <$inhandle2>){
+	$line =~ s/\r|\n//g; #Remove newlines, regardless of format
+	chomp $line;
+	my @list = split(" ", $line);
+	$fasta_id{$list[0]} = 1;
+}
+
+
+#Read fasta file, keep indicated fasta.	
+my $flag = 0;	#flag = 1 means keep
+while (my $line = <$inhandle1>){
+	$line =~ s/\r|\n//g; #Remove newlines, regardless of format
+	chomp $line;
+	if (substr($line, 0,1) eq '>' ){  #header
+			$flag = 0;
+			my @list = split(/\>| /, $line);
+				if(exists $fasta_id{$list[1]}){
+			  print $outhandle $line, "\n";
+			  $flag = 1;
+			}
+	}else{
+		if($flag == 1){
+			print $outhandle $line, "\n";
+		}
+	}
+	}
