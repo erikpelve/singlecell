c4e44190c68d1375927038b0ff5c9dbc5b615e22
erikpelve <erik.pelve@gmail.com>
2014-05-07 19:23:53 -0400

    Update create_multi_fasta_for_multiple_species.pl
    Ready for debug

--- create_multi_fasta_for_multiple_species.pl
+++ create_multi_fasta_for_multiple_species.pl
@@ -8,12 +8,26 @@ use warnings;
 
 
 
-my $outfile = "gene.".$ARGV[0].".fasta";
+my $outfile = $ARGV[0].".fasta";
 open my $outhandle, '>', $outfile or die "Couldn't write to the file $outfile\n";
 
 
+#Loop first file, store fasta file names and genes
+my $infile_list = $ARGV[0];
+open my $inhandle_list, '<', $infile_list or die "Couldn't open the file $infile_list\n";
+
+my %gene_list; #filename and gene
+
+while(my $line = <$inhandle_list>){
+	chomp $line;	
+	my @list = split(/\t/, $line);
+	$gene_list{$list[0]} = $list[1];
+	}
 
 
+
+
+#for each fasta file
 my $n = 0;
 
 foreach(@ARGV){
@@ -21,18 +35,36 @@ foreach(@ARGV){
 	next if ($n == 1); #skip first attribute, where the number is stored.
 	my $infile1 = $_;
 	open my $inhandle1, '<', $infile1 or die "Couldn't open the file $infile1\n";
+	my $gene_name = $genelist{$infile1};
+	my @namelist = split(/\_/, $infile1);
+	my $m;
+                        if ($n <= 9){
+                                $m = "00".$n;
+                                }
+                        elsif($n <= 99){
+                                $m = "0".$n;
+                                }
+                        elsif($n <= 999){
+                                $m = $n;
+                                }
+                        elsif($n == 1000){
+                                die "Warning, maximum 999 sequences can be processed.\n";
+                                }
 
-	my $m = 0;
+	my $newname = substr($namelist[0], 0,1).substr($namelist[1],0,5).$m;
+
+
+
 	my $flag = 0;
+
+	#loop fasta file
 	while (my $line = <$inhandle1>){
-	chomp $line;
-#	my @list = split(" ", $line);
-		if (substr($line, 0,1) eq '>' ){
-			$m++;
+		chomp $line; 
+		if (substr($line, 0,1) eq '>' ){ #header
 			$flag = 0;
-			if ($m == $ARGV[0]){
+			if ($line eq $gene_name){
 				$flag = 1;
-				print $outhandle $line, "\n";
+				print $outhandle $newname, "\n";
 			}else{}
 		}else{
 			if ($flag == 1){
