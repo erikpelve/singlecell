67f42d7f07565c71a51ccc621a8c25eedf5fc22d
erikpelve <erik.pelve@gmail.com>
2014-06-04 23:13:38 -0400

    Update concatinate_fasta_aligns2.pl
    Done

--- concatinate_fasta_aligns2.pl
+++ concatinate_fasta_aligns2.pl
@@ -13,10 +13,10 @@ use warnings;
 
 my $prefix = $ARGV[0];
 
-my $outfile = $prefix."concatinated.align.fasta";
+my $outfile = $prefix.".concatinated.align.fasta";
 open my $outhandle, '>', $outfile or die "Couldn't open the file $outfile\n";
 
-my $outfile2 = "gene_index".$prefix."concatinated.align.fasta";
+my $outfile2 = "gene_index.".$prefix.".concatinated.align.fasta";
 open my $outhandle_index, '>', $outfile2 or die "Couldn't open the file $outfile2\n";
 
 my %gene_list; #filename and geneseq
@@ -26,9 +26,10 @@ my %gene_list; #filename and geneseq
 my $index_position = 0;
 my $n = 0;
 foreach(@ARGV){
-	next if($n == 0);
 	$n++;
+	next if($n == 1);
 	
+
 	my $infile1 = $_;
 
 	my @infile_list = split(/\//, $infile1);
@@ -39,25 +40,25 @@ foreach(@ARGV){
 
 	my $gene_name;
 	my @tempseq;
-	
+
 	my $index_position_plus1 = $index_position +1;
-	
+
 	# print index file
-	print $outhandle_index "DNA", "\t", $infile_parts[0], "\t" $index_position_plus1 , "-"; # print the line for the coming gene, except the end coordinate
+	print $outhandle_index "DNA", "\t", $infile_parts[0], "\t", $index_position_plus1 , "-"; # print the line for the coming gene, except the end coordinate
 
 
 	#loop fasta file
 	my $flag = 0;
 	my $gene_no = 0;
 
-	
+
 
 	while (my $line = <$inhandle1>){
 		chomp $line;
 		if (substr($line, 0,1) eq '>' ){ #header
 			if ($flag ==1){
 				my $seq = join("\n",@tempseq);
-				$index_position = $index_position + length($line);
+				$index_position = $index_position + length($line) if ($gene_no ==0);
 				print $outhandle_index $index_position, "\n" if ($gene_no == 0);;
 				$gene_no++;
 				my $seqcul;
@@ -72,9 +73,9 @@ foreach(@ARGV){
 				}
 			$gene_name = $line;
 		}else{
-			
-			
-			
+
+
+
 			$flag =1;
 			push(@tempseq, $line);
 			$index_position = $index_position + length($line) if ($gene_no == 0);
